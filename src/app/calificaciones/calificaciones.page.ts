888932e5231ff742f9b566cf20904dd37f5e189e
maik2597 <michael.bello1997@gmail.com>
2019-05-21 22:19:53 -0400

    asdasd

--- src/app/calificaciones/calificaciones.page.ts
+++ src/app/calificaciones/calificaciones.page.ts
@@ -9,23 +9,23 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class CalificacionesPage implements OnInit {
   punto: string;
-  constructor(public alertctrl: AlertController, public nav : NavController) { this.punto ="1" }
+  constructor(public alertctrl: AlertController, public nav: NavController) { this.punto = "1" }
 
   ngOnInit() {
   }
-  async votocs(){
+  async votocs() {
     let alert = await this.alertctrl.create({
-          header:'Calificacion',
-          subHeader: 'Has votado un: '+this.punto+' Gracias por la calificacion',
-          mode:"ios",
-          buttons: [{
-            text:'Ok',
-            handler:(ok)=>{
-              this.nav.back();
-            }
-          }]
-        })
-        alert.present();
-      }
-    
+      header: 'Calificacion',
+      subHeader: 'Has votado un: ' + this.punto + ' Gracias por la calificacion',
+      mode: "ios",
+      buttons: [{
+        text: 'Ok',
+        handler: (ok) => {
+          this.nav.back();
+        }
+      }]
+    })
+    alert.present();
+  }
+
 }
--- src/app/help/help.page.ts
+++ src/app/help/help.page.ts
@@ -9,18 +9,18 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class HelpPage implements OnInit {
   punto: string;
-  constructor(public alertctrl: AlertController, public nav : NavController) { this.punto ="1" }
+  constructor(public alertctrl: AlertController, public nav: NavController) { this.punto = "1" }
 
   ngOnInit() {
   }
-  async votocs(){
-let alert = await this.alertctrl.create({
-      header:'Calificacion',
-      subHeader: 'Has votado un: '+this.punto+' Gracias por la calificacion',
-      mode:"ios",
+  async votocs() {
+    let alert = await this.alertctrl.create({
+      header: 'Calificacion',
+      subHeader: 'Has votado un: ' + this.punto + ' Gracias por la calificacion',
+      mode: "ios",
       buttons: [{
-        text:'Ok',
-        handler:(ok)=>{
+        text: 'Ok',
+        handler: (ok) => {
           this.nav.back();
         }
       }]
